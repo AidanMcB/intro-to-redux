f30084cb90bdc4c0e12dd6b13cd92a8c149f4f3a
Aidan McBride <aidankmcbride@gmail.com>
2021-05-20 22:37:09 -0400

    remade

---
+++ redux-basic/src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+// STORE => globalized state
+
+// ACTION=> descsribes what you want to do ex:'increment'
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+// REDUCER
+//takes state and action
+const counter = (state = 0, action) => {
+  switch(action.type){
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state -1;
+  }
+}
+//add action to store 
+let store = createStore(counter);
+//Display in the console
+store.subscribe(() => console.log(store.getState()));
+
+
+// DISPATCH
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
