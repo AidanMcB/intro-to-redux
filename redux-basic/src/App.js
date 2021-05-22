7b62f499cfa7c4c7d15dc35fc566a96468a71a0d
Aidan McBride <aidankmcbride@gmail.com>
2021-05-22 18:54:23 -0400

    dispatch with arguments

--- redux-basic/src/App.js
+++ redux-basic/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 
 function App() {
+
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector( state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      
-      
+      <h1> Counter {counter}</h1>
+      <button onClick={()=> dispatch(increment(5))}>+</button>
+      <button onClick={()=> dispatch(decrement(5))}>-</button>
+
+      {isLogged ? 
+      <h3>Hidden Info. Cannot see if not logged in</h3>
+      : null }
     </div>
   );
 }
